028aef244c1f71d314af0ab13e93ab8e51d2d56c
Sai Krishna Undurthi <sai.undurthi@gmail.com>
2015-11-05 20:06:44 -0800

    Activation updated

--- SavingCityApp/db/schema.rb
+++ SavingCityApp/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104092612) do
+ActiveRecord::Schema.define(version: 20151104202944) do
 
   create_table "characters", force: :cascade do |t|
     t.string   "character_name"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20151104092612) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
