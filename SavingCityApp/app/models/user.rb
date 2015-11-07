771463fe09afa672c475bff4222cad7b1fd6ebd4
Sai Krishna Undurthi <sai.undurthi@gmail.com>
2015-11-06 17:21:17 -0800

    Fixed Account activations

--- SavingCityApp/app/models/user.rb
+++ SavingCityApp/app/models/user.rb
@@ -42,11 +42,7 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
-  # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
-  end
+  
 
 # Activates an account.
   def activate
