4477fdc1aa594bccdf3cfc651b57f21e37f3346d
rujoota <rujoota.shah@gmail.com>
2015-12-05 23:23:29 -0800

    updated home pages with bootstrap

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,8 +2,9 @@ class AccountActivationsController < ApplicationController
 
 	def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.activate
+    #if user && !user.activated? && user.authenticated?(:activation, params[:id])
+    if user && user.authenticated?(:activation, params[:id])
+      #user.activate
       log_in user
       flash[:success] = "Account activated!"
       redirect_to user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
+      #@user.send_activation_email
+      #flash[:info] = "Please check your email to activate your account."
+      flash[:info] = "Success in user registration!"
       redirect_to root_url
     else
       render 'new'
